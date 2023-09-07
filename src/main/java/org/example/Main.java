cd0abb9adfaa1c2e38ddac88b1ae2b41ed6192c1
FaridaFatali <faridafatali.91@gmail.com>
2023-09-07 15:57:58 +0400

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * Practice: <a href="https://www.youtube.com/watch?v=wk1Fbqh7Tew">...</a>
+ * @author Farida Fatali
+ * Executable Quiz Game in Java
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        Quiz quiz = new Quiz();
+    }
+}
